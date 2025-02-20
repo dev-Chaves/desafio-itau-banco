da3dc4526467d42f9ae18e3510008b1ede61f95c
chaves <chavesprogrammer@gmail.com>
2025-02-20 10:52:58 -0300

    adição dos endpoints funcionando!!!

--- src/main/java/com/desafiounibanco/desafio_itau_backend/controller/TransacoesController.java
+++ src/main/java/com/desafiounibanco/desafio_itau_backend/controller/TransacoesController.java
@@ -1,15 +1,33 @@
 package com.desafiounibanco.desafio_itau_backend.controller;
 
+import com.desafiounibanco.desafio_itau_backend.domain.Transacao;
+import com.desafiounibanco.desafio_itau_backend.dto.TransacaoDto;
+import com.desafiounibanco.desafio_itau_backend.service.TransacaoServices;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/transacoes")
 public class TransacoesController {
 
-   @PostMapping(value = "transacao")
-   public ResponseEntity<Object> Teste (){
-       return null;
-   }
+    private final TransacaoServices transacaoServices;
+
+    public TransacoesController(TransacaoServices transacaoServices) {
+        this.transacaoServices = transacaoServices;
+    }
+
+    @GetMapping("/listar")
+    ResponseEntity<List<Transacao>> list(){
+        return new ResponseEntity<>(transacaoServices.listTransacao(), HttpStatus.OK);
+    }
+
+    @PostMapping("/transacao")
+    public ResponseEntity<Transacao> makeTransition(@RequestBody TransacaoDto transacaoDto ){
+
+        return transacaoServices.makeTransition(transacaoDto);
+    };
 
 }
