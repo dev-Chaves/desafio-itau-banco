b6dfe14a2091c7c5e548750ef72a33c43b0bebd3
chaves <chavesprogrammer@gmail.com>
2025-02-21 14:41:50 -0300

    estatisticas dto + método para pegar estatisticas

---
+++ src/main/java/com/desafiounibanco/desafio_itau_backend/dto/EstatisticasDto.java
@@ -0,0 +1,8 @@
+package com.desafiounibanco.desafio_itau_backend.dto;
+
+public record EstatisticasDto(long qtd,
+                              Double sum,
+                              Double avg,
+                              Double min,
+                              Double max) {
+}
